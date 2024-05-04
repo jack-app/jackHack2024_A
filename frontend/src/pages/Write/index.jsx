db3390f2cd743ed8248531f7ae99eaa620a23111
fukushimataiki <fukushimataiki@fukushimataikinoMacBook-Pro.local>
2024-05-04 18:02:29 +0900

    write画面＂

--- frontend/src/pages/Write/index.jsx
+++ frontend/src/pages/Write/index.jsx
@@ -4,15 +4,25 @@ import style from "./index.module.css";
 function Write() {
   return (
     <div>
-      <div>Write</div>
+       <Link to="/calendar">
+       <img src="back_button.png"></img>
+       </Link>
+      <div>20XX/XX</div>
       <nav className={style.navigation}>
         <Link to="/write">write</Link>
         <Link to="/detail">detail</Link>
         <Link to="/calendar">calendar</Link>
         <Link to="/">home</Link>
+        <textarea></textarea>
+        <Link to="/calendar">
+        <img src="kakutei_bottom.png"></img>
+       </Link>
       </nav>
     </div>
   );
 }
+
+
+
 
 export default Write;
