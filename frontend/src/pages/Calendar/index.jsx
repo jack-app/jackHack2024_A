cc078056002986d05089c8296e5d534e4ec8d40b
suke-0612 <ousuke612@gmail.com>
2024-05-06 00:47:17 +0900

    日付がずれていたのを修正

--- frontend/src/pages/Calendar/index.jsx
+++ frontend/src/pages/Calendar/index.jsx
@@ -1,23 +1,30 @@
 import { Link, useNavigate } from "react-router-dom";
 import style from "./index.module.css";
-import RCalendar from "react-calendar"; //名前の衝突を避けるためにRCalendarとしてimport
-import "react-calendar/dist/Calendar.css";  //カレンダーのCSS
-// import { useHistory } from "react-router-dom";
+import RCalendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function Calendar() {
   const navigate = useNavigate();
-  const today = new Date().toISOString().slice(0, 10);
+  const today = modifyDate(new Date());
+  const formattedDate = today.toISOString().slice(0, 10);
+
+  function modifyDate(date) {
+    const currentDate = date || new Date();
+    const nextDayTimestamp = currentDate.getTime() + (24 * 60 * 60 * 1000);
+    return new Date(nextDayTimestamp);
+  }
   const handleDayClick = (date) => {
-    navigate(`/detail?date=${encodeURIComponent(date.toISOString().slice(0, 10))}`);
+    const modifiedDate = modifyDate(date);
+    const formattedSelectedDate = modifiedDate.toISOString().slice(0, 10);
+    navigate(`/detail?date=${encodeURIComponent(formattedSelectedDate)}`);
   };
+
   return (
     <div>
-      <h2 className={style.topDate}>{today}</h2>
+      <h2 className={style.topDate}>{formattedDate}</h2>
       <h2>月間の状況</h2>
       <div className={style.RCalendarContainer}>
-        <RCalendar
-          onClickDay={(date) => handleDayClick(date)}
-        />
+        <RCalendar onClickDay={(date) => handleDayClick(date)} />
       </div>
       <div>
         <div className={style.aorichanContainer}>
@@ -28,7 +35,7 @@ function Calendar() {
         </div>
         <span className={style.aorichanExplain}>扇のキャラクター アオリちゃん</span>
         <div className={style.rightArrowIcon}>
-          <Link to={`/write?date=${encodeURIComponent(today)}`} >
+          <Link to={`/write?date=${encodeURIComponent(formattedDate)}`}>
             <img src="right-arrow-icon.svg" />
           </Link>
         </div>
