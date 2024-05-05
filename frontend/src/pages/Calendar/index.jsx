dd110ea78021e7501b111f1b7e6ab6c035b9df2d
suke-0612 <ousuke612@gmail.com>
2024-05-05 09:31:56 +0900

    [WIP]RCalendar置いた

--- frontend/src/pages/Calendar/index.jsx
+++ frontend/src/pages/Calendar/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import style from "./index.module.css";
-// import RCalendar from "react-calendar";
+import RCalendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";  //カレンダーのCSS
+import { useState } from "react";
+
 function Calendar() {
+  const [value, setValue] = useState();
   return (
     <div>
       <div>Calendar</div>
-      {/* <RCalendar /> */}
+      <RCalendar
+        value={value}
+        onClickDay={(e) => { setValue(e); console.log(e); }}
+      />
       <nav className={style.navigation}>
         <Link to="/write">write</Link>
         <Link to="/detail">detail</Link>
