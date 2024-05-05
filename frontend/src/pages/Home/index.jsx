ca87d21521d964e3df89463eec29e75b0565a286
lan36999 <sakasakoto@yahoo.co.jp>
2024-05-05 15:58:10 +0900

    ボタンの最適化

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,28 +3,20 @@ import style from "./index.module.css";
 import { AuthContextConsumer } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-
 function Home() {
   const { loginUser, login, logout } = AuthContextConsumer();
   const navigate = useNavigate();
-
   useEffect(() => {
     if (loginUser) {
       navigate("/calendar");
     }
   }, [loginUser, navigate]);
-
   return (
     <div>
-      <div>Home</div>
-      <nav className={style.navigation}>
-        <Link to="/write">write</Link>
-        <Link to="/detail">detail</Link>
-        <Link to="/calendar">calendar</Link>
-        <Link to="/">home</Link>
-      </nav>
+      <div>Your Stock</div>
+      <div className={style.logo}><img src="logo.jpg"/></div>
+      <button className={style.login}></button>
     </div>
   );
 }
-
-export default Home;
+export default Home
