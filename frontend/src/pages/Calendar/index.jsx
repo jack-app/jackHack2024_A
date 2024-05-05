14e316df3552abc189e1fe2bf38e0bb169615449
suke-0612 <ousuke612@gmail.com>
2024-05-05 12:26:39 +0900

    WIP

--- frontend/src/pages/Calendar/index.jsx
+++ frontend/src/pages/Calendar/index.jsx
@@ -1,24 +1,33 @@
 import { Link } from "react-router-dom";
 import style from "./index.module.css";
-import RCalendar from "react-calendar";
+import RCalendar from "react-calendar"; //名前の衝突を避けるためにRCalendarとしてimport
 import "react-calendar/dist/Calendar.css";  //カレンダーのCSS
 import { useState } from "react";
 
 function Calendar() {
   const [value, setValue] = useState();
+  const today = new Date().toLocaleDateString();
+
   return (
     <div>
-      <div>Calendar</div>
+      <h3>{today} 月間の状況だぞ</h3>
       <RCalendar
         value={value}
         onClickDay={(e) => { setValue(e); console.log(e); }}
       />
-      <nav className={style.navigation}>
-        <Link to="/write">write</Link>
-        <Link to="/detail">detail</Link>
-        <Link to="/calendar">calendar</Link>
-        <Link to="/">home</Link>
-      </nav>
+      <div>
+        <div>
+          <img src="aorichan.png" />
+          <p>扇のキャラクター アオリちゃん</p>
+        </div>
+        <dir>
+          <p>日記を書くぞ</p>
+          <Link>
+            <img src="right-arrow-icon.svg" />
+          </Link>
+        </dir>
+      </div>
+
     </div>
   );
 }
